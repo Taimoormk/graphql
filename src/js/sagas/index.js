9ceff6e7b8d38d970ce7f35135c948764defc919
Taimoor Khan <taimoor.m.k@live.com>
2018-06-30 10:51:18 +0500

    intial configuration

---
+++ src/js/sagas/index.js
@@ -0,0 +1,18 @@
+// ########## Import Dependencies Here ##########
+import { takeLatest, all, fork } from 'redux-saga/effects';
+
+// ########## Import Components Here ##########
+
+// ########## Import Sagas Here ##########
+import * as types from '../constants';
+import testSaga from './testSaga';
+
+function* watchTestAction() {
+  yield takeLatest(types.TEST, testSaga);
+}
+
+export default function* rootSaga() {
+  yield all([
+    fork(watchTestAction)
+  ]);
+}
